7784a36782a27c396a0f3b797a9b584c222fc9f6
Andrii Vasyliev <sol@hiqdev.com>
2018-03-16 11:39:09 +0200

    redone creating extension to separate migration

---
+++ src/migrations/m180305_000000_extensions.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace transmedia\signage\file\sql\migrations;
+
+/**
+ * Class m180305_000000_extensions
+ */
+class m180305_000000_extensions extends \hiqdev\reodb\yii\migrations\m180201_000000_extensions
+{
+}
