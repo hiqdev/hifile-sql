a40c188236a4bd505f8524c4ca1c8cb1525b9abf
Andrii Vasyliev <sol@hiqdev.com>
2018-03-26 17:00:20 +0300

    added `fill.sql` with test data

---
+++ src/sql/fill.sql
@@ -0,0 +1,3 @@
+--- FILE
+SELECT add_file(hstore(ARRAY['client_id', 12345, 'provider', 'filestack', 'remoteid', 'test-remoteid-000001']::text[]));
+SELECT add_file(hstore(ARRAY['client_id', 12345, 'provider', 'filestack', 'remoteid', 'ttc9Vr9SjybPZ0W3Frys']::text[]));
